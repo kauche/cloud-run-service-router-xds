c3f271cb36db05105c4ba0fb6ea2553ba0c75c24
Yuki Ito <mrno110y@gmail.com>
2022-11-21 00:09:26 +0900

    feat: implement driver/event/subscriber

--- internal/driver/event/subscriber/service_event_subscriber.go
+++ internal/driver/event/subscriber/service_event_subscriber.go
@@ -2,23 +2,32 @@ package subscriber
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
 
 	"github.com/kauche/cloud-run-service-router-xds/internal/usecase"
 )
 
 type ServiceEventSubscriber struct {
-	uc *usecase.ServiceUseCase
+	uc     *usecase.ServiceUseCase
+	logger logr.Logger
 }
 
-func NewServiceEventSubscriber(uc *usecase.ServiceUseCase) *ServiceEventSubscriber {
+func NewServiceEventSubscriber(uc *usecase.ServiceUseCase, logger logr.Logger) *ServiceEventSubscriber {
 	return &ServiceEventSubscriber{
-		uc: uc,
+		uc:     uc,
+		logger: logger,
 	}
 }
 
-func (s *ServiceEventSubscriber) ServicesRefreshedEventHandler(ctx context.Context) error {
+func (s *ServiceEventSubscriber) ServicesRefreshedEventHandler() error {
+	s.logger.Info("refreshing services")
+
+	ctx := context.Background()
+
 	if err := s.uc.DistributeServices(ctx); err != nil {
-		// TODO:
+		return fmt.Errorf("failed to distribute services: %w", err)
 	}
 
 	return nil

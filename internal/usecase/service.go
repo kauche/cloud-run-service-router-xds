dcb12a54a94ef956592de929b9efa9920879310e
Yuki Ito <mrno110y@gmail.com>
2022-11-21 00:13:11 +0900

    feat: implement usecase

--- internal/usecase/service.go
+++ internal/usecase/service.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kauche/cloud-run-service-router-xds/internal/domain/distributor"
 	"github.com/kauche/cloud-run-service-router-xds/internal/domain/event"
@@ -25,11 +26,24 @@ func NewServiceUseCase(broker event.ServiceEventBroker, distributor distributor.
 func (u *ServiceUseCase) DistributeServices(ctx context.Context) error {
 	services, err := u.repository.ListAllServices(ctx)
 	if err != nil {
-		// TODO:
+		return fmt.Errorf("failed to list services: %w", err)
 	}
 
 	if err := u.distributor.DistributeServices(ctx, services); err != nil {
-		// TODO:
+		return fmt.Errorf("failed to distribute services: %w", err)
+	}
+
+	return nil
+}
+
+func (u *ServiceUseCase) DistributeServicesToClient(ctx context.Context, client string) error {
+	services, err := u.repository.ListAllServices(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to list services: %w", err)
+	}
+
+	if err := u.distributor.DistributeServicesToClient(ctx, services, client); err != nil {
+		return fmt.Errorf("failed to distribute services to the client `%s`: %w", client, err)
 	}
 
 	return nil
@@ -37,11 +51,19 @@ func (u *ServiceUseCase) DistributeServices(ctx context.Context) error {
 
 func (u *ServiceUseCase) RefreshServices(ctx context.Context) error {
 	if err := u.repository.RefreshServices(ctx); err != nil {
-		// TODO:
+		return fmt.Errorf("failed to refresh services: %w", err)
 	}
 
 	if err := u.broker.PublishServicesRefreshedEvent(ctx); err != nil {
-		// TODO:
+		return fmt.Errorf("failed to publish serivce refreshed event: %w", err)
+	}
+
+	return nil
+}
+
+func (u *ServiceUseCase) RegisterClientToDistributor(ctx context.Context, client string, serviceNames []string) error {
+	if err := u.distributor.RegisterClient(ctx, client, serviceNames); err != nil {
+		return fmt.Errorf("failed to add client to the distributor: %w", err)
 	}
 
 	return nil

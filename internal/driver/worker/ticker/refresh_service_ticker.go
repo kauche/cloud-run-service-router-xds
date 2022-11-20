a09f7b669f4aa00157987c6c734fa9cf32a2c42b
Yuki Ito <mrno110y@gmail.com>
2022-11-21 00:12:43 +0900

    feat: implement driver/worker/ticker

--- internal/driver/worker/ticker/refresh_service_ticker.go
+++ internal/driver/worker/ticker/refresh_service_ticker.go
@@ -2,28 +2,48 @@ package ticker
 
 import (
 	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-logr/logr"
 
 	"github.com/kauche/cloud-run-service-router-xds/internal/usecase"
 )
 
 type ServiceRefreshTicker struct {
-	uc *usecase.ServiceUseCase
+	uc         *usecase.ServiceUseCase
+	logger     logr.Logger
+	syncPeriod time.Duration
 }
 
-func NewServiceRefreshTicker(uc *usecase.ServiceUseCase) *ServiceRefreshTicker {
+func NewServiceRefreshTicker(uc *usecase.ServiceUseCase, syncPeriod time.Duration, logger logr.Logger) *ServiceRefreshTicker {
 	return &ServiceRefreshTicker{
-		uc: uc,
+		uc:         uc,
+		logger:     logger,
+		syncPeriod: syncPeriod,
 	}
 }
 
 func (t *ServiceRefreshTicker) tick(ctx context.Context) error {
 	if err := t.uc.RefreshServices(ctx); err != nil {
-		// TODO:
+		return fmt.Errorf("failed to refresh services: %s", err)
 	}
 
 	return nil
 }
 
 func (t *ServiceRefreshTicker) Start(ctx context.Context) error {
-	return nil
+	ticker := time.NewTicker(t.syncPeriod)
+
+	for {
+		if err := t.tick(ctx); err != nil {
+			t.logger.Error(err, "failed to tick")
+		}
+
+		select {
+		case <-ticker.C:
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }

38e0df50b4114198ab72d6d7a0fbd1e3a7d10bd0
Yuki Ito <mrno110y@gmail.com>
2022-11-21 00:28:25 +0900

    feat: implement command

--- go.mod
+++ go.mod
@@ -3,22 +3,44 @@ module github.com/kauche/cloud-run-service-router-xds
 go 1.19
 
 require (
+	cloud.google.com/go/run v0.3.0
 	github.com/110y/run v1.0.1
 	github.com/110y/servergroup v0.0.0-20220503060600-9c5533f51f4a
-	github.com/envoyproxy/go-control-plane v0.10.3
+	github.com/envoyproxy/go-control-plane v0.10.3-0.20221109183938-2935a23e638f
+	github.com/go-logr/logr v1.2.3
+	github.com/go-logr/zapr v1.2.3
+	github.com/golang/protobuf v1.5.2
+	github.com/google/uuid v1.3.0
+	github.com/kauche/gopubsub v0.0.0-20221106083025-8fa1f9abd11f
+	github.com/samber/lo v1.35.0
+	go.uber.org/zap v1.23.0
+	google.golang.org/api v0.102.0
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
 )
 
 require (
+	cloud.google.com/go v0.105.0 // indirect
+	cloud.google.com/go/compute v1.12.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
+	cloud.google.com/go/longrunning v0.1.1 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
 	github.com/cncf/xds/go v0.0.0-20220314180256-7f1daf1720fc // indirect
 	github.com/envoyproxy/protoc-gen-validate v0.6.7 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.6.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
 	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220329172620-7be39ac1afc7 // indirect
-	google.golang.org/grpc v1.45.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221027153422-115e99e71e1c // indirect
 )
--- internal/command/command.go
+++ internal/command/command.go
@@ -2,17 +2,29 @@ package command
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	"github.com/110y/run"
 	"github.com/110y/servergroup"
 
 	"github.com/kauche/cloud-run-service-router-xds/internal/driver/db/cloudrun"
 	"github.com/kauche/cloud-run-service-router-xds/internal/driver/distributor/xds"
-	"github.com/kauche/cloud-run-service-router-xds/internal/driver/event/broker/channel"
+	"github.com/kauche/cloud-run-service-router-xds/internal/driver/event/broker/gopubsub"
 	"github.com/kauche/cloud-run-service-router-xds/internal/driver/event/subscriber"
+	"github.com/kauche/cloud-run-service-router-xds/internal/driver/flag/flag"
 	"github.com/kauche/cloud-run-service-router-xds/internal/driver/handler/grpc"
+	"github.com/kauche/cloud-run-service-router-xds/internal/driver/log/zap"
 	"github.com/kauche/cloud-run-service-router-xds/internal/driver/worker/ticker"
 	"github.com/kauche/cloud-run-service-router-xds/internal/usecase"
+)
+
+const (
+	exitCodeFailedToCreateLogger                    = 100
+	exitCodeFailedToCreateCloudRunClient            = 101
+	exitCodeFailedToSubscribeServicesRefreshedEvent = 102
+	exitCodeFailedToGetFlags                        = 103
+	exitCodeServerAborted                           = 200
 )
 
 func Run() {
@@ -20,32 +32,58 @@ func Run() {
 }
 
 func server(ctx context.Context) int {
-	sc := xds.NewSnapshotCache()
+	logger, err := zap.NewLogger()
+	if err != nil {
+		_, ferr := fmt.Fprintf(os.Stderr, "failed to create a logger: %s", err)
+		if ferr != nil {
+			// Unhandleable, something went wrong...
+			panic(fmt.Sprintf("failed to write log:`%s` original error is:`%s`", ferr, err))
+		}
+		return exitCodeFailedToCreateLogger
+	}
+
+	commandLogger := logger.WithName("command")
+
+	sc := xds.NewSnapshotCache(logger.WithName("snapshot_cache"))
 
 	sd := xds.NewServiceDistributor(sc)
 
-	sb := channel.NewServiceEventBroker()
+	sb := gopubsub.NewServiceEventBroker(logger.WithName("service_event_broker"))
 
-	sr := cloudrun.NewServiceRepository()
+	flags, err := flag.GetFlags()
+	if err != nil {
+		commandLogger.Error(err, "failed to get flags")
+		return exitCodeFailedToGetFlags
+	}
+
+	sr, err := cloudrun.NewServiceRepository(ctx, flags.Project, flags.Location)
+	if err != nil {
+		commandLogger.Error(err, "failed to create a cloud run client")
+		return exitCodeFailedToCreateCloudRunClient
+	}
 
 	uc := usecase.NewServiceUseCase(sb, sd, sr)
 
-	ss := subscriber.NewServiceEventSubscriber(uc)
+	ss := subscriber.NewServiceEventSubscriber(uc, logger.WithName("service_event_subscriber"))
 
-	st := ticker.NewServiceRefreshTicker(uc)
+	st := ticker.NewServiceRefreshTicker(uc, flags.SyncPeriod, logger.WithName("service_refresh_ticker"))
 
-	gs := grpc.NewServer(ctx, sc)
+	gs := grpc.NewServer(ctx, uc, sc, flags.Port, logger.WithName("grpc_server"))
 
 	if err := sb.SubscribeServicesRefreshedEvent(ctx, ss.ServicesRefreshedEventHandler); err != nil {
-		// TODO:
+		commandLogger.Error(err, "failed to subscribe the service refreshed event")
+		return exitCodeFailedToCreateCloudRunClient
 	}
 
 	var sg servergroup.Group
+
 	sg.Add(st)
 	sg.Add(gs)
+	sg.Add(sb)
 
 	if err := sg.Start(ctx); err != nil {
-		// TODO:
+		commandLogger.Error(err, "the server has aborted")
+		return exitCodeServerAborted
 	}
 
 	return 0
